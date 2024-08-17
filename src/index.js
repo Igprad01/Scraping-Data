77953c219214c591a9084e892a28e1dcb14c5693
igprad01 <igustiputra99@gmail.com>
2024-08-17 12:58:52 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import express from 'express';
+import fs from 'fs';
+
+const app = express();
+const port = 3000;
+
+const url = '';
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(url);
+        const data = response.data
+        fs.writeFileSync("data.json", JSON.stringify(data, null, 1));
+
+        res.status(200).send(`data sudah ada`);
+    } catch (error) {
+        res.status(500).send("data gagal");
+    }
+
+});
+
+app.listen(port, () => {
+    console.log(`localhost ${port}`);
+})
+
+
+
+
+
+
+
