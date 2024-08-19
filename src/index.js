8d9213891fe8d80cca9adea75363048bb9daf174
igprad01 <igustiputra99@gmail.com>
2024-08-19 20:26:27 +0700

    delete configuration express js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
 import axios from "axios";
-import express from 'express';
 import fs from 'fs';
 
-const app = express();
-const port = 3000;
 
-const url = '';
+const url = 'https://www.octaveclothing.com/men';
 
-app.listen(port, () => {
-    console.log(`localhost ${port}`);
-});
-
-app.get("/", async (req, res) => {
+const ScrapData = async () => {
     try {
         const response = await axios.get(url);
         const data = response.data;
 
+        
+
       fs.writeFileSync("data.json", JSON.stringify(data, null, 5));
-        res.status(200).send(`data sudah ada`);
         console.log('data berhasil di send');
     } catch (error) {
-        res.status(500).send(`${error.message}`);
         console.log(error.message);
     }
+}
 
-});
+export default ScrapData()
 
-
-// nanti dibuat function untuk mengambil data tertentu aja dari scrap web
 
 
 
