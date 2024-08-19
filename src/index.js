4aabe20d07d716854d0ede0ea33331ecfd0535c3
igprad01 <igustiputra99@gmail.com>
2024-08-19 21:15:51 +0700

    add lib cheerio and fix push data.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import axios from "axios";
-import fs from 'fs';
-
+const axios = require('axios');
+const fs = require('fs');
+const cheerio = require('cheerio');
 
 const url = 'https://www.octaveclothing.com/men';
 
@@ -9,25 +9,27 @@ const ScrapData = async () => {
         const response = await axios.get(url);
         const data = response.data;
 
+        const Load = cheerio.load(data);
+
+        const ScrapData = []
+
+        Load('.product-card').each((index, element) => {
+              const title = Load(element).find('h6').text().trim();
+              const price = Load(element).find('span').first().text().trim();
+
+              ScrapData.push({
+                title,
+                price,
+              });
+        }); 
+
         
 
-      fs.writeFileSync("data.json", JSON.stringify(data, null, 5));
+      fs.writeFileSync("data.json", JSON.stringify(ScrapData, null, 5));
         console.log('data berhasil di send');
     } catch (error) {
         console.log(error.message);
     }
-}
+};
 
-export default ScrapData()
-
-
-
-
-
-
-
-
-
-
-
-
+ScrapData();
